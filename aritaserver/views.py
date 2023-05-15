f26bab25cc1c52ed25aace30b867b8a6ba9c3cdc
Victor Morales <vmmc100@gmail.com>
2023-05-15 19:06:06 -0400

    prueba

--- aritaserver/views.py
+++ aritaserver/views.py
@@ -1,4 +1,6 @@
 from django.http import FileResponse, HttpRequest, HttpResponse
 
 def wordSplitter(request):
+    archivo = open("hola.txt", 'w')
+    archivo.close()
     return FileResponse(open('goku.zip', 'rb'))
